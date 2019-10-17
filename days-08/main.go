0f4836e423c57c5c7b7efabdffe5c4f60665ba06
Redstone Zhao <zhaoxiongfei@nequal.com>
2019-10-17 01:27:35 +0000

    增加一个斐波那契数列闭包的解决方案

--- days-08/main.go
+++ days-08/main.go
@@ -10,9 +10,27 @@ func adder() func(int) int {
 	}
 }
 
+func fibonacci() func() uint64 {
+	var c uint64 // 上一个值
+	var n uint64 // 当前值
+	c = 1
+	n = 0
+	return func() uint64 {
+		t := c
+		c = n
+		n += t
+		return n
+	}
+}
+
 func main() {
 	pos, neg := adder(), adder()
 	for i := 0; i < 10; i++ {
 		fmt.Println("pos:", pos(i), "neg:", neg(-2*i))
+	}
+
+	fib := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println("Fibonacci", i, "=", fib())
 	}
 }
