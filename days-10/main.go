7998e7a31e7d53d5953b377baf2fb2d1eb88a3e9
Redstone Zhao <zhaoxiongfei@nequal.com>
2019-10-17 01:52:14 +0000

    类型的方法，定义接受者为指针类型，方法内部可以修改接受者的值

---
+++ days-10/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X *= f
+	v.Y *= f
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := &Vertex{3, 4}
+	fmt.Println("before scale: v =", v, "abs =", v.Abs())
+	v.Scale(5)
+	fmt.Println("after scale: v =", v, "abs =", v.Abs())
+}
