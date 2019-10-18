7e0c6da1282ecf1fd2851d1462da96085ea7a327
Redstone Zhao <zhaoxiongfei@nequal.com>
2019-10-18 06:49:46 +0000

    增加程序的说明文字

--- days-20/main.go
+++ days-20/main.go
@@ -21,3 +21,19 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+/*
+Web 服务器
+包 http 通过任何实现了 http.Handler 的值来响应 HTTP 请求：
+
+package http
+
+type Handler interface {
+    ServeHTTP(w ResponseWriter, r *Request)
+}
+在这个例子中，类型 Hello 实现了 `http.Handler`。
+
+访问 http://localhost:4000/ 会看到来自程序的问候。
+
+注意： 这个例子无法在基于 web 的指南用户界面运行。为了尝试编写 web 服务器，可能需要安装 Go。
+*/
